05dde76d64c4a04d67ccb7da67eb8459f37bfc0c
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-10 21:53:09 -0600

    add function email validation and search person by email

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+// GET DATA FROM data.json file
 const DATA = data;
 
-console.log(DATA);
+// ------------------------------------------------------------------------
+// RECEIVE AN INPUT ELEMENT AND CHECK IF IS VALID
+// ------------------------------------------------------------------------
+function emailInputIsValid(input) {
+    return input.value !== '' && input.checkValidity();
+}
+
+// ------------------------------------------------------------------------
+// SEARCH AN PERSON WITH THE EMAIL PASSED ACROSS DATA ARRAY
+// ------------------------------------------------------------------------
+function searchPersonByEmail(email) {
+
+    // READ EACH PERSON OF DATA ARRAY
+    for (let person of DATA) {
+
+        //CHECK IF HIS EMAIL MATCH WITH SEARCHED EMAIL
+        if (person.email === email) {
+            // IF MATCH RETURN THAT PERSON
+            return person;
+        }
+    }
+    // IF CYCLE END AND NEVER MATCH MEANS EMAIL NO EXIST
+    return undefined;
+}
