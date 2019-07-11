4d83747c0cfd41b97d7e879e910af6ed25ea3f00
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-10 23:15:51 -0600

    integrate show.js

--- js/search.js
+++ js/search.js
@@ -62,20 +62,3 @@ function searchEmail(input_id) {
 
     // IF INPUT EMAIL IS INVALID DO NOTHING
 }
-
-// ------------------------------------------------------------------------
-// GET THE ITEM PERSON FROM LOCAL STORAGE
-// ------------------------------------------------------------------------
-function getPerson() {
-    return localStorage.getItem('person')
-}
-
-// WRITE A GIVEN TEXT IN AN GIVEN HTML ELEMENT TAKING HIS STYLE
-function writeTextInElement(elementId, text) {
-
-    // GET THE ELEMENT WITH THE GIVEN ID
-    var element = document.getElementById(elementId);
-
-    // WRITE THE TEXT
-    element.innerText = text;
-}
