10468c6a8e2dd91b6a1adf3608233c4ca2f49299
Josue Gabuardi <josuegabuardi@gmail.com>
2019-07-10 23:12:08 -0600

    add new file show.js

--- js/search.js
+++ js/search.js
@@ -69,3 +69,13 @@ function searchEmail(input_id) {
 function getPerson() {
     return localStorage.getItem('person')
 }
+
+// WRITE A GIVEN TEXT IN AN GIVEN HTML ELEMENT TAKING HIS STYLE
+function writeTextInElement(elementId, text) {
+
+    // GET THE ELEMENT WITH THE GIVEN ID
+    var element = document.getElementById(elementId);
+
+    // WRITE THE TEXT
+    element.innerText = text;
+}
